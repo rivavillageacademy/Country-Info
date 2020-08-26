9ac4661c9c93aad4cceeb6b179f6889bbb8c1806
rivavillageacademy <69723115+rivavillageacademy@users.noreply.github.com>
2020-08-26 20:02:38 +0400

    Add files via upload

---
+++ Country Info/Country Info/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Country Info
+//
+//  Created by Muhamed Alkhatib on 26/08/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
